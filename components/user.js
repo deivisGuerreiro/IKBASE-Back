99db39e824f5dccf932f55319f05cf238d9721e7
Deivis Felipe <deivis.guerreiro@gmail.com>
2019-12-19 11:32:41 -0300

    arrumando umas rotas

--- components/user.js
+++ components/user.js
@@ -8,36 +8,37 @@ const encripta = senha => {
 }
 
 //INSERIR
-const insert = async (nome,email,senha) => {
-  
+const insert = async (nome, email, senha) => {
+
   const query = "INSERT INTO usuarios (nome,email,senha) VALUES ($1,$2,$3)"
-  result = await db.query(query,[nome,email,encripta(senha)])
-  console.log(result.rows);
-  return result.rows
-  
+  result = await db.query(query, [nome, email, encripta(senha)])
+  const query2 = "SELECT * FROM usuarios WHERE nome = $1 and email = $2"
+  result2 = await db.query(query2, [nome, email])
+  return result2.rows[0].id
+
 }
 
 //VERIFICA SE USUÁRIO EXISTE
 const userExists = async (email) => {
-  
-    const query = "SELECT * FROM usuarios WHERE email = $1"
-    const userDoBanco = await db.query(query,[email])
-    if(userDoBanco.rowCount > 0) {
-      return true
-    } else return false
+
+  const query = "SELECT * FROM usuarios WHERE email = $1"
+  const userDoBanco = await db.query(query, [email])
+  if (userDoBanco.rowCount > 0) {
+    return true
+  } else return false
 }
 
 //EDITAR
-const update = async (nome,email,senha, id) => {
-  
+const update = async (nome, email, senha, id) => {
+
   const verifica = "SELECT * FROM usuarios WHERE id = $1"
-  var result = await db.query(verifica,[id])
-  if(!result.rows.length > 0){
+  var result = await db.query(verifica, [id])
+  if (!result.rows.length > 0) {
     console.log("ITEM INEXISTENTE");
     return "ITEM INEXISTENTE"
   }
   const query = "UPDATE usuarios SET nome = $1, email = $2, senha = $3 WHERE id=$4 RETURNING *"
-  result = await db.query(query,[nome,email,encripta(senha),id])
+  result = await db.query(query, [nome, email, encripta(senha), id])
 
   console.log(result.rows);
   return result.rows
@@ -47,23 +48,23 @@ const update = async (nome,email,senha, id) => {
 const deletar = async (id) => {
 
   const verifica = "SELECT * FROM usuarios WHERE id = $1"
-  var result = await db.query(verifica,[id])
-  if(!result.rows.length > 0){
+  var result = await db.query(verifica, [id])
+  if (!result.rows.length > 0) {
     console.log("ITEM INEXISTENTE");
     return "ITEM INEXISTENTE"
   }
   const query = "DELETE FROM usuarios WHERE id=$1"
-  await db.query(query,[id])
+  await db.query(query, [id])
 
   console.log("ITEM DELETADO");
   return "ITEM DELETADO"
-  
+
 }
 //BUSCAR POR ID
 const get = async (id) => {
   var result
   const query = "SELECT * FROM usuarios WHERE id = $1";
-  result = await db.query(query,[id])
+  result = await db.query(query, [id])
 
   return result.rows
 }
@@ -78,7 +79,7 @@ const getByName = async (nome) => {
 }
 
 //BUSCAR TODOS
-const getAll = async() => {
+const getAll = async () => {
   var result
   const query = " SELECT * FROM  usuarios"
 
@@ -88,16 +89,26 @@ const getAll = async() => {
 }
 
 //lOGIN
-const login = async(email, senha) => {
+const login = async (email, senha) => {
   let result
-  
+
   const query = "SELECT u.email, u.senha AS senha FROM usuarios AS u WHERE u.email = $1"
-   result = await db.query(query, [email])
-   if(result.rowCount > 0 && bcrypt.compareSync(senha, result.rows[0].senha)) {
-    return true
+  result = await db.query(query, [email])
+  if (result.rowCount > 0 && bcrypt.compareSync(senha, result.rows[0].senha)) {
+    const query2 = "SELECT * FROM usuarios WHERE  email = $1"
+    result2 = await db.query(query2, [email])
+    return result2.rows[0]
   } else return false
 }
 
 module.exports = {
-  getAll,insert,update,deletar,get, encripta, userExists, login, getByName
-} 
+  getAll,
+  insert,
+  update,
+  deletar,
+  get,
+  encripta,
+  userExists,
+  login,
+  getByName
+}
