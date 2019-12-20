1471894076d07f49b64c735d45b59831c0cdb1a4
Procurados do FBI <guerreirodosul01@gmail.com>
2019-12-20 19:19:10 -0300

    arrumei umas consultas

--- conexao.js
+++ conexao.js
@@ -1,8 +1,15 @@
 const pg = require("pg")
 
-const client = new pg.Client({
+/*const client = new pg.Client({
   user: 'dilsinho',
   host: '69.171.4.30',
+  database:'talentos1',
+  password:'talentos2019',
+  port:5432,
+})*/
+const client = new pg.Client({
+  user: 'dilsinho',
+  host: '127.0.0.1',
   database:'talentos1',
   password:'talentos2019',
   port:5432,
