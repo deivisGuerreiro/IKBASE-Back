1d1960bd37b110bb414fa7be228d65af4ee73d4b
Procurados do FBI <guerreirodosul01@gmail.com>
2019-12-20 22:52:20 -0300

    banco

--- conexao.js
+++ conexao.js
@@ -9,7 +9,7 @@ const pg = require("pg")
 })*/
 const client = new pg.Client({
   user: 'dilsinho',
-  host: '127.0.0.1',
+  host: '69.171.4.30',
   database:'talentos1',
   password:'talentos2019',
   port:5432,
